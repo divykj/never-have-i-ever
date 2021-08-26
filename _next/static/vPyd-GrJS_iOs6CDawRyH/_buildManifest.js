1d05d0aa88cd788f3b91d8a7b5d6237e512ac339
divykj <divykj@users.noreply.github.com>
2021-08-26 14:29:54 +0000

    🚀 💚 fix gh pages deployment b679b6eb937d4b94c8ffb34041ba0e19f0e4685d

---
+++ _next/static/vPyd-GrJS_iOs6CDawRyH/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/386-a504c99b612707fa1f41.js","static/chunks/pages/index-69b085d37ea5a009b2c5.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();

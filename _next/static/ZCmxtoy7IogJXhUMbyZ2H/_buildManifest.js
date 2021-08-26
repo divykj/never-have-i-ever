43bdeb69cac4d484466818c9d929e96e71b81af2
divykj <divykj@users.noreply.github.com>
2021-08-26 15:15:26 +0000

    🚀 ♻️ cleanup code dc795fe706d143c2059757b0a39d6c20b650e44e

--- _next/static/ZCmxtoy7IogJXhUMbyZ2H/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/386-a504c99b612707fa1f41.js","static/chunks/pages/index-c693df2977f2627ac5f5.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();

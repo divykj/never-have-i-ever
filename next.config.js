28293e6b35de4a8175a7eabb0cadb603c02c7178
Divy Jain <dkj@somaiya.edu>
2021-09-02 21:59:51 +0530

    📦 add bundle size analyzer

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
-module.exports = {
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
+module.exports = withBundleAnalyzer({
   assetPrefix:
     process.env.NODE_ENV === "production" ? "/never-have-i-ever/" : "",
-};
+});
